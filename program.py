d2c023f2a3453d7e0b430beb956059b04d7e08ea
Guillaume Collic <gcollic@gmail.com>
2019-07-24 18:12:18 +0200

    Python -> v3

--- program.py
+++ program.py
@@ -1,12 +1,11 @@
-from ctypes import *
+from ctypes import cdll
 
 
 lib = cdll.LoadLibrary("./libffi-example.so")
 
 lib.example_init()
 
-for x in xrange(10):
-    print "Haskell fib(%d): %d" % (x, lib.fibonacci_hs(x))
+for x in range(10):
+    print(f"Haskell fib{x}: {lib.fibonacci_hs(x)}")
 
 lib.example_exit()
-
